693510e5a003ccd4d256a9ead5462b9e5617f245
Joe McGill <joemcgill@gmail.com>
2021-04-05 18:08:03 -0500

    Add `@alley-interactive/scaffold` package with `scaffold block` command
    This creates a new package for scaffolding features based on command line options. This commit includes the first subcommand, `scaffold block`, which will create a new folder wherever the command is run with block files created from templates in the `templates/block` directory.
    
    Templates are generated from mustache and are passed values from the command line options.

---
+++ packages/scaffold/lib/block.js
@@ -0,0 +1,70 @@
+const glob = require( 'fast-glob' );
+const makeDir = require( 'make-dir' );
+const { fromPairs, pickBy } = require( 'lodash' );
+const { dirname, join } = require( 'path' );
+const { readFile } = require( 'fs' ).promises;
+const { render } = require( 'mustache' );
+const { writeFile } = require( 'fs' ).promises;
+
+const getTemplates = async () => {
+  const path = join( __dirname, 'templates', 'block' );
+  const outputTemplatesFiles = await glob('**/*.mustache', {
+    cwd: path,
+    dot: true,
+  })
+  return fromPairs(
+		await Promise.all(
+			outputTemplatesFiles.map( async ( outputTemplateFile ) => {
+				const outputFile = outputTemplateFile.replace(
+					/\.mustache$/,
+					''
+				);
+				const outputTemplate = await readFile(
+					join( path, outputTemplateFile ),
+					'utf8'
+				);
+				return [ outputFile, outputTemplate ];
+			} )
+		)
+	);
+}
+
+const renderTemplates = async ( args ) => {
+  console.log('renderArgs', args);
+  const { slug } = args;
+  const templates = await getTemplates();
+
+  console.log(templates);
+  await Promise.all(
+    Object.keys(templates).map( async outputFile => {
+      const outputFilePath = join(
+        slug,
+        outputFile
+      );
+      await makeDir( dirname( outputFilePath ) );
+      writeFile(
+        outputFilePath,
+        render( templates[ outputFile ], args )
+      );
+    })
+  );
+}
+
+module.exports = args => {
+  // Make sure all values are defined.
+  const optionsValues = pickBy(
+    {
+      ...args
+    },
+    ( value ) => value !== undefined
+  );
+
+  const parsedArgs = {
+    ...optionsValues
+    // ...getFromConfig() @todo
+  };
+
+  console.log( 'parsed', parsedArgs );
+
+  renderTemplates( parsedArgs );
+};
---
+++ packages/scaffold/lib/index.js
@@ -0,0 +1,33 @@
+
+const { Command } = require('commander');
+const scaffoldBlock = require('./block');
+
+const program = new Command();
+
+/**
+ * Block scaffolding sub-command.
+ */
+program.command('block')
+  .arguments('<slug>')
+  .description('Create a block.')
+  .option( '--category <name>', 'category name for the block', 'layout' )
+  .option( '--dynamic <value>', 'is the block dynamic', true )
+  .option( '--namespace <value>', 'internal namespace for the block name', 'alley-blocks'  )
+  .option( '--short-description <value>', 'short description for the block', '' )
+  .option( '--text-domain <value>', 'text domain for the block', 'alley-blocks' )
+  .option( '--title <value>', 'display title for the block', 'Block Title' )
+  .action( (slug, options) => {
+    // Combine args and options into a single object.
+    const args = {
+      slug,
+      description: options.shortDescription,
+      isDynamic: options.dynamic,
+      ...options
+    }
+
+    console.log('args', args);
+    scaffoldBlock(args);
+  })
+
+// Parse all arguments.
+program.parse();
