1f0319f7e110e52f437c523cec83fb514aa8f0e7
Sean Fisher <srtfisher@gmail.com>
2023-03-27 17:09:04 -0400

    Scaffolding out a plugin

---
+++ plugin.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Plugin Name: Alley Scripts
+ * Plugin URI: https://github.com/alleyinteractive/alley-scripts
+ * Description: A development plugin for working on the Alley Scripts project.
+ * Version: 0.1.0
+ * Author: Alley
+ * Author URI: https://alley.com/
+ * Requires at least: 5.9
+ * Tested up to: 5.9
+ *
+ * @package Alley_Scripts
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
