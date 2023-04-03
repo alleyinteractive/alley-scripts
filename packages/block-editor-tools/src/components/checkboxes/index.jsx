1926037935fe6abe1976bd44dc55ea2cb53de047
Sean Fisher <srtfisher@gmail.com>
2023-04-03 16:53:47 -0400

    One more component docs and ignoring

--- packages/block-editor-tools/src/components/checkboxes/index.jsx
+++ packages/block-editor-tools/src/components/checkboxes/index.jsx
@@ -2,6 +2,11 @@ import PropTypes from 'prop-types';
 
 import { BaseControl, CheckboxControl } from '@wordpress/components';
 
+/**
+ * Provides a UI component that uses the `Checkbox` component to render the
+ * equivalent of a multi-select without needing to use the `SelectControl`
+ * component.
+ */
 const Checkboxes = ({
   label,
   value,
@@ -29,12 +34,24 @@ const Checkboxes = ({
 );
 
 Checkboxes.propTypes = {
+  /**
+   * The label for the component.
+   */
   label: PropTypes.string.isRequired,
+  /**
+   * The current value(s) as an array of strings.
+   */
   value: PropTypes.arrayOf(PropTypes.string).isRequired,
+  /**
+   * Available options to choose from with a structure of `{ value: '', label: '' }`.
+   */
   options: PropTypes.arrayOf(PropTypes.shape({
     label: PropTypes.string.isRequired,
     value: PropTypes.string.isRequired,
   })).isRequired,
+  /**
+   * Function called with the selected options after the user makes an update.
+   */
   onChange: PropTypes.func.isRequired,
 };
 
