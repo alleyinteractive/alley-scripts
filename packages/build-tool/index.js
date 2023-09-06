05675b2647f49afd97a7ed0595f578c1901cc6bc
Steven Slack <stevenslack@gmail.com>
2023-09-05 23:35:06 -0400

    add main index.js executable

--- packages/build-tool/index.js
+++ packages/build-tool/index.js
@@ -1,3 +1,49 @@
-const config = require('./config/webpack.config.js');
+#!/usr/bin/env node
+const { spawn } = require('node:child_process');
+const { argv, cwd } = require('node:process');
 
-module.exports = config;
+const { getArgFromCLI, getWebpackConfig, hasArgInCLI } = require('./utils');
+
+/**
+ * The default arguments to pass to wp-scripts.
+ *
+ * @type {string[]}
+ */
+const defaultArgs = [];
+
+/**
+ * If the `build` or `start` command is used, add the necessary wp-scripts args.
+ */
+if (hasArgInCLI('build') || hasArgInCLI('start')) {
+  defaultArgs.push(`--config=${getWebpackConfig()}`);
+
+  if (!hasArgInCLI('--webpack-copy-php')) {
+    defaultArgs.push('--webpack-copy-php');
+  }
+
+  /**
+   * The default directory where wp-scripts will detect block.json files.
+   * Explicitly set the webpack source directory to "blocks" unless specified.
+   *
+   * @see https://github.com/WordPress/gutenberg/tree/trunk/packages/scripts#automatic-blockjson-detection-and-the-source-code-directory
+   *
+   * @type {string|null}
+   */
+  const webpackSrcDir = hasArgInCLI('--webpack-src-dir')
+    ? getArgFromCLI('--webpack-src-dir') : 'blocks';
+
+  defaultArgs.push(`--webpack-src-dir=${webpackSrcDir}`);
+}
+
+spawn(
+  'wp-scripts',
+  // Pass all arguments after the first two `wp-scripts build` to wp-scripts.
+  [
+    ...argv.slice(2),
+    ...defaultArgs,
+  ],
+  {
+    cwd: cwd(),
+    stdio: 'inherit',
+  },
+);
