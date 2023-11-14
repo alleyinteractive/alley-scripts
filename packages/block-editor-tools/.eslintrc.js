99dee1c742f31cc8a221ce93beabc878b0d5c9b5
Steven Slack <stevenslack@gmail.com>
2023-11-14 14:15:05 -0500

    disable import/no-extraneous-dependencies

--- packages/block-editor-tools/.eslintrc.js
+++ packages/block-editor-tools/.eslintrc.js
@@ -11,6 +11,15 @@ module.exports = {
     'import/no-cycle': 'off',
     'react/require-default-props': 'off',
     'react/function-component-definition': 'off',
+    /**
+     * The following rule is disabled because many of the packages used
+     * are @wordpress/* packages and other packages that are listed
+     * as externals in the Dependency Extraction Webpack Plugin.
+     * Since these packages are not used in the built files but instead referenced as globals,
+     * they do not need to be listed in the dependency array. This will cause the rule,
+     * 'import/no-extraneous-dependencies' to throw an error.
+     */
+    'import/no-extraneous-dependencies': 'off',
   },
   settings: {
     'import/resolver': 'webpack',
