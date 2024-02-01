2a2c80dfd16c27d0a47f32063bac385061e7679e
Sean Fisher <srtfisher@gmail.com>
2024-02-01 17:12:03 -0500

    Include create-wordpress-plugin by default

--- packages/scaffolder/src/defaultConfiguration.ts
+++ packages/scaffolder/src/defaultConfiguration.ts
@@ -5,7 +5,22 @@ import type { Configuration } from './types';
  * Default configuration of the scaffolder.
  */
 export const DEFAULT_CONFIGURATION: Configuration = typeof jest === 'undefined' ? {
-  features: [],
+  // TODO: Move these to a separate NPM package.
+  features: [{
+    name: 'create-wordpress-plugin',
+    type: 'repository',
+    inputs: [{
+      name: 'name',
+      description: 'The name of the plugin',
+      type: 'string',
+      required: true,
+    }],
+    repository: {
+      github: 'alleyinteractive/create-wordpress-plugin',
+      destination: '{{ dasherize inputs.name }}',
+      postCloneCommand: 'php configure.php',
+    },
+  }],
   sources: [],
 } : {
   features: [],
