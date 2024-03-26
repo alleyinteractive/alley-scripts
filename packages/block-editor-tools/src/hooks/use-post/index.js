2ea39bdd4da3b726c01451c71a0f328ef94adeab
Travis Weston <anubisthejackle@users.noreply.github.com>
2024-03-26 16:22:26 -0400

    Add an options parameter to override context

--- packages/block-editor-tools/src/hooks/use-post/index.js
+++ packages/block-editor-tools/src/hooks/use-post/index.js
@@ -5,11 +5,12 @@ import { useSelect } from '@wordpress/data';
  *
  * @param {int}    postId   The ID for the post to return.
  * @param {string} postType Optional. The post type to select. Default 'post'.
+ * @param {object} options Optional object containing parameters to pass to getEntityRecord.
  * @returns {object} An object containing a hasResolved property
  *                   and the returned post object.
  */
-const usePost = (postId, postType = 'post') => useSelect(
-  (select) => select('core').getEntityRecord('postType', postType, postId, { context: 'view' }),
+const usePost = (postId, postType = 'post', options = { context: 'view' }) => useSelect(
+  (select) => select('core').getEntityRecord('postType', postType, postId, options),
   [postId, postType],
 );
 
