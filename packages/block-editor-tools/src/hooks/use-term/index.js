d7d515466764f5f00ea47b1ed07af6291ca92282
Renato Alves <19148962+renatonascalves@users.noreply.github.com>
2024-04-05 10:21:20 -0300

    `useTerm` passes the `options` as a param

--- packages/block-editor-tools/src/hooks/use-term/index.js
+++ packages/block-editor-tools/src/hooks/use-term/index.js
@@ -6,11 +6,12 @@ import { useSelect } from '@wordpress/data';
  *
  * @param {int}    termId   The ID for the term to return.
  * @param {string} taxonomy Optional. The taxonomy name. Defaults to 'category'.
+ * @param {object} options Optional object containing parameters to pass to getEntityRecord.
  * @returns {object} An object containing a hasResolved property
  *                   and the returned term object.
  */
-const useTerm = (termId, taxonomy = 'category') => useSelect(
-  (select) => select(store).getEntityRecord('taxonomy', taxonomy, termId, { context: 'view' }),
+const useTerm = (termId, taxonomy = 'category', options = { context: 'view' }) => useSelect(
+  (select) => select(store).getEntityRecord('taxonomy', taxonomy, termId, options),
   [termId, taxonomy],
 );
 
