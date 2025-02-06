b1b5257e3a544ba65e01f265f5b099c2c6bc8193
efuller <eric@alley.co>
2025-02-06 08:58:24 -0500

    Initial conversion to TS for TermSelector

---
+++ packages/block-editor-tools/src/components/term-selector/index.tsx
@@ -0,0 +1,55 @@
+// Dependencies.
+import { __ } from '@wordpress/i18n';
+import { Selector } from '..';
+
+interface SelectedTerms {
+  id: number;
+  title: string;
+  type: string;
+  url: string;
+}
+
+interface TermSelectorProps {
+  className?: string;
+  emptyLabel?: string;
+  label?: string;
+  maxPages?: number;
+  multiple?: boolean;
+  onSelect: (terms: SelectedTerms[]) => void;
+  placeholder?: string;
+  subTypes?: string[];
+  selected?: SelectedTerms[];
+  threshold?: number;
+}
+
+/**
+ * Render term selector component.
+ */
+const TermSelector = ({
+  className = '',
+  emptyLabel = __('No terms found', 'alley-scripts'),
+  label = __('Search for terms', 'alley-scripts'),
+  maxPages = 5,
+  multiple = false,
+  onSelect,
+  placeholder = __('Search for terms', 'alley-scripts'),
+  subTypes = [],
+  selected = [],
+  threshold = 3,
+}: TermSelectorProps) => (
+  <Selector
+    type="term"
+    className={className}
+    emptyLabel={emptyLabel}
+    label={label}
+    maxPages={maxPages}
+    multiple={multiple}
+    onSelect={onSelect}
+    placeholder={placeholder}
+    subTypes={subTypes}
+    selected={selected}
+    threshold={threshold}
+  />
+);
+
+export default TermSelector;
