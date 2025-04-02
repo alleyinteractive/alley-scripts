26790422158130db2a99d1d762224b4b76c378e6
efuller <eric@alley.co>
2025-04-02 11:12:46 -0400

    Create a helper function for generating the config to export

--- packages/build-tool/config/webpack.config.ts
+++ packages/build-tool/config/webpack.config.ts
@@ -15,7 +15,7 @@ interface WPScriptsConfig extends Configuration {
 }
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const defaultConfig: WPScriptsConfig = require('@wordpress/scripts/config/webpack.config');
+const wpScriptsConfig: WPScriptsConfig | WPScriptsConfig[] = require('@wordpress/scripts/config/webpack.config');
 
 /**
  * Check if the build is running in production mode.
@@ -35,9 +35,112 @@ const entriesDir: string = process.env.ENTRIES_DIRECTORY || 'entries';
 const blocksOnly: boolean = process.env.BLOCKS_ONLY === 'true';
 
 /**
+ * Whether to use experimental modules in the build.
+ */
+const experimentalModules: boolean = process.env.WP_EXPERIMENTAL_MODULES === 'true';
+
+/**
  * The mode to run webpack in. Either production or development.
  */
 const mode: string = isProduction ? 'production' : 'development';
+
+// const defaultConfig = Array.isArray(wpScriptsConfig) ? wpScriptsConfig[0] : wpScriptsConfig;
+
+/**
+ * Generate the base webpack configuration.
+ *
+ * @param defaultConfig - The default webpack configuration from @wordpress/scripts.
+ * @returns The generated webpack configuration.
+ */
+function generateBaseConfig(defaultConfig: WPScriptsConfig): Configuration {
+  return {
+    ...defaultConfig,
+
+    // Dynamically produce entries from the slotfills index file and all blocks.
+    entry: () => {
+      let blocks = typeof defaultConfig.entry === 'function' ? defaultConfig.entry() : {};
+      blocks = blocks && typeof blocks === 'object' ? blocks : {};
+
+      const entries = blocksOnly === true ? {} : getEntries(entriesDir);
+
+      return {
+        ...blocks,
+        ...entries,
+      };
+    },
+
+    /**
+     * This configuration option is being overridden from the default wp-scripts
+     * config in order to process the build so that each entry point is output
+     * to its own directory.
+     *
+     * The 'build' output path is maintained due to the
+     * devServer configuration from wp-scripts.
+     */
+    output: {
+      clean: mode === 'production' && !experimentalModules,
+
+      filename: (pathData: PathData) => processFilename(pathData, true, 'js'),
+      chunkFilename: (pathData: PathData) => processFilename(pathData, false, 'js', 'runtime'),
+      path: path.join(cwd(), 'build'),
+    },
+
+    // Configure plugins.
+    plugins: [
+      ...(Array.isArray(defaultConfig.plugins) ? defaultConfig.plugins : []),
+      new CopyWebpackPlugin({
+        patterns: [
+          {
+            from: '**/{index.php,*.css}',
+            context: entriesDir,
+            noErrorOnMissing: true,
+          },
+        ],
+      }),
+      new MiniCssExtractPlugin({
+        filename: (pathData: PathData) => processFilename(pathData, true, 'css'),
+        chunkFilename: (pathData: PathData) => processFilename(pathData, false, 'css', 'runtime'),
+      }),
+      new CleanWebpackPlugin({
+        cleanOnceBeforeBuildPatterns: [],
+        cleanAfterEveryBuildPatterns: [
+          /**
+           * Remove duplicate entry CSS files generated from default
+           * MiniCssExtractPlugin plugin in wpScripts.
+           *
+           * The default MiniCssExtractPlugin filename is [name].css
+           * resulting in the generation of the `${entriesDir}-*.css` files.
+           * The configuration in this file for MiniCssExtractPlugin outputs
+           * the entry CSS into the entry src directory name.
+           */
+          `${entriesDir}-*.css`,
+          // Maps are built when running the start mode with wpScripts.
+          `${entriesDir}-*.css.map`,
+        ],
+        protectWebpackAssets: false,
+      }),
+    ],
+
+    // This webpack alias rule is needed at the root to ensure that the paths are resolved
+    // using the custom alias defined below.
+    resolve: {
+      ...defaultConfig.resolve,
+      alias: {
+        ...defaultConfig?.resolve?.alias,
+        // Custom alias to resolve paths to the project root. Example: '@/client/src/index.js'.
+        '@': path.resolve(cwd()),
+      },
+    },
+
+    devServer: mode === 'production' ? {} : {
+      ...defaultConfig.devServer,
+      allowedHosts: 'all',
+      static: {
+        directory: '/build',
+      },
+    },
+  };
+}
 
 /**
  * webpack configuration.
@@ -49,91 +152,8 @@ const mode: string = isProduction ? 'production' : 'development';
  * @see https://github.com/WordPress/gutenberg/tree/trunk/packages/scripts#extending-the-webpack-config
  * @see https://github.com/WordPress/gutenberg/blob/trunk/packages/scripts/config/webpack.config.js
  */
-const config: Configuration = (defaultConfig ? {
-  ...defaultConfig,
-
-  // Dynamically produce entries from the slotfills index file and all blocks.
-  entry: () => {
-    let blocks = typeof defaultConfig.entry === 'function' ? defaultConfig.entry() : {};
-    blocks = blocks && typeof blocks === 'object' ? blocks : {};
-
-    const entries = blocksOnly === true ? {} : getEntries(entriesDir);
-
-    return {
-      ...blocks,
-      ...entries,
-    };
-  },
-
-  /**
-   * This configuration option is being overridden from the default wp-scripts
-   * config in order to process the build so that each entry point is output
-   * to its own directory.
-   *
-   * The 'build' output path is maintained due to the
-   * devServer configuration from wp-scripts.
-   */
-  output: {
-    clean: mode === 'production',
-
-    filename: (pathData: PathData) => processFilename(pathData, true, 'js'),
-    chunkFilename: (pathData: PathData) => processFilename(pathData, false, 'js', 'runtime'),
-    path: path.join(cwd(), 'build'),
-  },
-
-  // Configure plugins.
-  plugins: [
-    ...(Array.isArray(defaultConfig.plugins) ? defaultConfig.plugins : []),
-    new CopyWebpackPlugin({
-      patterns: [
-        {
-          from: '**/{index.php,*.css}',
-          context: entriesDir,
-          noErrorOnMissing: true,
-        },
-      ],
-    }),
-    new MiniCssExtractPlugin({
-      filename: (pathData: PathData) => processFilename(pathData, true, 'css'),
-      chunkFilename: (pathData: PathData) => processFilename(pathData, false, 'css', 'runtime'),
-    }),
-    new CleanWebpackPlugin({
-      cleanAfterEveryBuildPatterns: [
-        /**
-         * Remove duplicate entry CSS files generated from default
-         * MiniCssExtractPlugin plugin in wpScripts.
-         *
-         * The default MiniCssExtractPlugin filename is [name].css
-         * resulting in the generation of the `${entriesDir}-*.css` files.
-         * The configuration in this file for MiniCssExtractPlugin outputs
-         * the entry CSS into the entry src directory name.
-         */
-        `${entriesDir}-*.css`,
-        // Maps are built when running the start mode with wpScripts.
-        `${entriesDir}-*.css.map`,
-      ],
-      protectWebpackAssets: false,
-    }),
-  ],
-
-  // This webpack alias rule is needed at the root to ensure that the paths are resolved
-  // using the custom alias defined below.
-  resolve: {
-    ...defaultConfig.resolve,
-    alias: {
-      ...defaultConfig?.resolve?.alias,
-      // Custom alias to resolve paths to the project root. Example: '@/client/src/index.js'.
-      '@': path.resolve(cwd()),
-    },
-  },
-
-  devServer: mode === 'production' ? {} : {
-    ...defaultConfig.devServer,
-    allowedHosts: 'all',
-    static: {
-      directory: '/build',
-    },
-  },
-} : {});
+const config: Configuration | Configuration[] = (wpScriptsConfig && Array.isArray(wpScriptsConfig)
+  ? [generateBaseConfig(wpScriptsConfig[0]), wpScriptsConfig[1]]
+  : generateBaseConfig(wpScriptsConfig));
 
 export default config;
