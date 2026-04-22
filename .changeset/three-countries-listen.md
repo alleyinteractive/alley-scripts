d65e9e45e97d2957d39538c29d7661666f2cad2f
jessicamgoddard <43393533+jessicamgoddard@users.noreply.github.com>
2026-04-22 09:06:27 -0400

    683: Add textdomain to fix block.json value (#896)
    * 683: Add textdomain to fix block.json value
    
    * 683: Add changeset

---
+++ .changeset/three-countries-listen.md
@@ -0,0 +1,5 @@
+---
+"@alleyinteractive/create-block": minor
+---
+
+Addresses an issue in which the textdomain for block.json was the block name, instead of the plugin name.
--- packages/create-block/index.ts
+++ packages/create-block/index.ts
@@ -180,6 +180,12 @@ console.log(`🚀 ${chalk.underline(chalk.bold.green('@alleyinteractive/create-b
      */
     '--variant',
     'dynamic',
+    /**
+     * Set the textdomain to the namespace (plugin name) rather than the block slug.
+     * @see https://developer.wordpress.org/block-editor/reference-guides/packages/packages-create-block/#textdomain
+     */
+    '--textdomain',
+    namespace,
   ];
 
   // When a slug is provided, pass it as a positional argument to wp-create-block.
